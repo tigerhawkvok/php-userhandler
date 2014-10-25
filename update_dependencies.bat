d48b5c3fb220087f4cc0f9b2a58faa8f2b2473f4
Philip Kahn <tigerhawkvok@gmail.com>
2014-10-25 12:53:56 -0700

    Compile updates

--- update_dependencies.bat
+++ update_dependencies.bat
@@ -4,5 +4,4 @@ call bower update
 echo Minifying ...
 java -jar yuicompressor.jar bower_components/purl/purl.js -o js/purl.min.js
 java -jar yuicompressor.jar bower_components/jquery-cookie/jquery.cookie.js -o js/jquery.cookie.min.js
-
-cake watch
+java -jar yuicompressor.jar js/loadJQuery.js -o js/loadJQuery.min.js
