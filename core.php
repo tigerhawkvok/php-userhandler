d4f5de3a7b847dfedb003ffa05b208b0d1af54f3
Philip Kahn <tigerhawkvok@gmail.com>
2014-10-31 10:51:56 -0700

    Check for class existence

--- core.php
+++ core.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once(dirname(__FILE__)."/db/DBHelper.php");
-require_once(dirname(__FILE__)."/xml/xml.php");
+if(!class_exists("DBHelper"))
+  {
+    require_once(dirname(__FILE__)."/db/DBHelper.php"); 
+  }
+if(!class_exists("Xml"))
+  {
+    require_once(dirname(__FILE__)."/xml/xml.php");
+  }
 
 ?>
